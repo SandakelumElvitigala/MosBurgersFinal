0208c0da5137a7b3b088b2c2ebdef6da4734d0d2
Don Sandakelum Elvitigala <kalpaselvitigala@gmail.com>
2024-08-30 05:01:20 +0530

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,9 +1,8 @@
-// Function to get the value of the input field
+
 function getInputValue() {
-    // Get the input field element by its ID
+    
     const key = document.getElementById('passkey');
 
-    // Get the value of the input field
     const enteredPasskey = key.value;
 
     const pass = window.users.find(item => item.name === enteredPasskey) || window.users.find(item => item.num === enteredPasskey);
@@ -11,7 +10,7 @@ function getInputValue() {
     if (pass) {
         const name = pass.name;
         
-        window.location.href = `../home.html?name=${name}`;
+        window.location.href = `home.html?name=${name}`;
     }
 }
 
